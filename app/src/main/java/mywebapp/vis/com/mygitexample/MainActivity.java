caf7078abbdc412a2458970f2a74ad3e23021171
Haraprasad <haraprasadsatapathy@gmail.com>
2018-06-06 16:18:24 +0530

    Test

--- app/src/main/java/mywebapp/vis/com/mygitexample/MainActivity.java
+++ app/src/main/java/mywebapp/vis/com/mygitexample/MainActivity.java
@@ -15,8 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //uh88yguygyg
 
 
     }
